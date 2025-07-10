705fc11416dea5be1848d61bb2ea604e01867855
jonbov <jon@bovre.no>
2025-07-10 19:56:39 +0200

    s

--- ansible_sros/AWX/Python/GK_show_info.py
+++ ansible_sros/AWX/Python/GK_show_info.py
@@ -10,10 +10,6 @@ print(requests.get('http://awx.bovre.net:30080/api/login', auth=auth))
 
 url = "http://awx.bovre.net:30080/api/v2/job_templates/"
 #"name": "templates",
-print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-x = requests.get(url)
-print(x.status_code)
-print(x.text)
 
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 x = requests.get(url, auth = ('admin', 'pop4aN94'))
@@ -25,16 +21,11 @@ print(json_formatted_str)
 
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
-
-
-url = "http://awx.bovre.net:30080/api/v2/job_templates/24/launch/"
-#"name": "Show info 200""
-x = requests.post(url, auth = ('admin', 'pop4aN94'))
-
-print(x.text)
-obj = json.loads(x.text)
-json_formatted_str = json.dumps(obj, indent=4)
-print(json_formatted_str)
-
-print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-
+#https://www.askpython.com/python/api-calls-bearer-token-authentication
+#url = 'https://api.example.com/resource'
+headers = {
+    'Authorization': 'Bearer vSmeC2edaNaOviDbVFnHHoAOReUQOl',
+    'Content-Type': 'application/json'
+}
+response = requests.get(url, headers=headers)
+print(response.text)
