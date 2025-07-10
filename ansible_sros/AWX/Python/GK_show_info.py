2ff8b53d4d44ef4e6437da834f36ce843422793d
jonbov <jon@bovre.no>
2025-07-10 19:48:49 +0200

    s

--- ansible_sros/AWX/Python/GK_show_info.py
+++ ansible_sros/AWX/Python/GK_show_info.py
@@ -1,12 +1,14 @@
 import requests
 import pprint
+import json
 from requests.auth import HTTPBasicAuth
+
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 auth = HTTPBasicAuth('api', 'pop4aN94')
 print(requests.get('http://awx.bovre.net:30080/api/login', auth=auth))
 
 
-url = "http://awx.bovre.net:30080/api/v2/job_templates/24/"
+url = "http://awx.bovre.net:30080/api/v2/job_templates/"
 #"name": "templates",
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 x = requests.get(url)
@@ -17,6 +19,9 @@ print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 x = requests.get(url, auth = ('admin', 'pop4aN94'))
 print(x.status_code)
 print(x.text)
+obj = json.loads(x.text)
+json_formatted_str = json.dumps(obj, indent=4)
+print(json_formatted_str)
 
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
@@ -24,11 +29,12 @@ print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 url = "http://awx.bovre.net:30080/api/v2/job_templates/24/launch/"
 #"name": "Show info 200""
-x = requests.get(url, auth = ('admin', 'pop4aN94'))
-print(x.status_code)
+x = requests.post(url, auth = ('admin', 'pop4aN94'))
+
 print(x.text)
-pprint.pprint(x.text)
-pretty_json_str = pprint.pformat(x.text, compact=True).replace("'",'"')
+obj = json.loads(x.text)
+json_formatted_str = json.dumps(obj, indent=4)
+print(json_formatted_str)
 
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
